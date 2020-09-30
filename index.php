86ad8c936ac5b5b723de0deb5de03691ffcb805e
William Chen <seed45699@gmail.com>
2020-10-01 00:03:00 +0800

    Finish index.php layout

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/reset.css" />
+  <link rel="stylesheet" href="./css/index.css" />
+  <title>Blog</title>
+</head>
+
+<body>
+  <nav>
+    <ul class="blog-navlist">
+      <li class="logo">Who's Blog</li>
+      <li>文章列表</li>
+      <li>分類專區</li>
+      <li>關於我</li>
+    </ul>
+    <ul class="blog-navlist">
+      <li>管理後臺</li>
+      <li>登出</li>
+    </ul>
+  </nav>
+  <div class="blog-topic">
+    <h1>存放技術之地</h1>
+    <span>Welcome to my blog</span>
+  </div>
+  <div class="blog-wrapper">
+    <div class="blog-block__group">
+      <div class="blog-block">
+        <div class="blog-block__header">
+          <div class="blog-block__title">
+            嗨～歡迎來到程式新手村 feat. 胡斯的異想世界
+          </div>
+          <button class="blog-block__edit">編輯</button>
+        </div>
+        <div class="blog-block__time">2020/07/01 10:15</div>
+        <div class="blog-block__content">
+          羅曼·羅蘭在不經意間這樣說過<br />
+          郭沫若說過一句富有哲理的話，形成天才的決定因素應該是勤奮。這句話語雖然很短，但令我浮想聯翩。每個人都不得不面對這些問題。在面對這種問題時，
+          我認為，
+          生活中，若中午吃什麼出現了，我們就不得不考慮它出現了的事實。中午吃什麼，發生了會如何，不發生又會如何。我們一般認為，抓住了問題的關鍵，其他一切則會迎刃而解。我們都知道，只要有意義，那麼就必須慎重考慮。現在，解決中午吃什麼的問題，是非常非常重要的。所以，
+          那麼， 一般來講，我們都必須務必慎重的考慮考慮
+        </div>
+        <button class="blog-block__read-more">READ MORE</button>
+      </div>
+      <div class="blog-block">
+        <div class="blog-block__header">
+          <div class="blog-block__title">
+            嗨～歡迎來到程式新手村 feat. 胡斯的異想世界
+          </div>
+          <button class="blog-block__edit">編輯</button>
+        </div>
+        <div class="blog-block__time">2020/07/01 10:15</div>
+        <div class="blog-block__content">
+          羅曼·羅蘭在不經意間這樣說過<br />
+          郭沫若說過一句富有哲理的話，形成天才的決定因素應該是勤奮。這句話語雖然很短，但令我浮想聯翩。每個人都不得不面對這些問題。在面對這種問題時，
+          我認為，
+          生活中，若中午吃什麼出現了，我們就不得不考慮它出現了的事實。中午吃什麼，發生了會如何，不發生又會如何。我們一般認為，抓住了問題的關鍵，其他一切則會迎刃而解。我們都知道，只要有意義，那麼就必須慎重考慮。現在，解決中午吃什麼的問題，是非常非常重要的。所以，
+          那麼， 一般來講，我們都必須務必慎重的考慮考慮
+        </div>
+        <button class="blog-block__read-more">READ MORE</button>
+      </div>
+    </div>
+  </div>
+  <footer>Copyright © 2020 Who's Blog All Rights Reserved.</footer>
+</body>
+
+</html>
