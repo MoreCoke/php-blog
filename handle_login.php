93017c6ebd3f75cd6b342baaf833ada7e5501474
William Chen <seed45699@gmail.com>
2020-10-07 00:36:48 +0800

    Feat user data
    先在資料庫新增一筆使用者帳密，密碼用 hash 存

--- handle_login.php
+++ handle_login.php
@@ -1,3 +1,14 @@
 <?php
+require_once('conn.php');
 
-echo $_POST['username'];
+
+$username = $_POST['username'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$sql = 'INSERT INTO morecoke_blog_users (username, password) VALUE (?,?)';
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('ss', $username, $password);
+$result = $stmt->execute();
+if (!$result) {
+  die(header('Location: login.php?errCode=1'));
+}
+header('Location: index.php');
